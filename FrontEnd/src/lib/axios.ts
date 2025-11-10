2dfc0dd8d7f9266f55afd18a72ccfbf2c5160c0d
Võ Nhật Trường Duy <duyvo1066@gmail.com>
2025-11-10 16:49:56 +0700

    fix: error when build

--- FrontEnd/src/lib/axios.ts
+++ FrontEnd/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:3000/api" : "/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
